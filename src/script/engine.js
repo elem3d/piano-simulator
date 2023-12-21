8047d25858737edc6c35ae3bcd7c0577a6299b1f
elem3d <elem3d@users.noreply.github.com>
2023-12-21 20:24:59 -0300

    fix tunes &keys

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,8 +1,14 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
-let audio = new Audio('tunes')
+let audio = new Audio('src/tunes1/a.wav')
 
-const playTune = (key) => {};
+const playTune = (key) => {
+    audio.src = `src/tunes1/${key}.wav`
+    if(key === 't'){
+        audio.volume = 0.5
+    }
+    audio.play()
+};
 
 pianoKeys.forEach((key) => {
     key.addEventListener('click', () => playTune(key.dataset.key))
