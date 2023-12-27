07c1fad256682c56f664043c51a30ce93860b831
elem3d <elem3d@users.noreply.github.com>
2023-12-27 13:25:01 -0300

    fix keyboard-keys | no play at same time

--- src/script/engine.js
+++ src/script/engine.js
@@ -8,7 +8,6 @@ let mapedKeys = [];
 let audio = new Audio('src/tunes/a.wav');
 
 var playedKey;
-let playedKeys = 0;
 
 pianoKeys.forEach((key) => {
     key.addEventListener('click', () => playTune(key.dataset.key))
@@ -29,52 +28,9 @@ const playTune = (key) => {
     playedKey = key
 };  
 
-function Channel(audioSrc) {
-	this.audioRef = audioSrc;
-    this.resource = new Audio(`${audioSrc}`)
-}
-
-Channel.prototype.play = function() {
-	// Try refreshing the resource altogether
-	this.resource.play();
-}
-function Switcher(audioSrc, num) {
-	this.channels = [];
-	this.num = num;
-	this.index = 0;
-
-	for (var i = 0; i < num; i++) {
-		this.channels.push(new Channel(audioSrc));
-	}
-    console.log(this.channels)
-}
-
-Switcher.prototype.play = function() {
-	this.channels[this.index++].play();
-	this.index = this.index < this.num ? this.index : 0;
-}
-
-function getAudioSrc(audios, key) {
-    audios.forEach((audioSrc) => {
-        if (audioSrc === `src/tunes/${key}.wav`) {
-        
-            console.log(audioSrc)
-
-            return audioSrc
-        }
-    })                    
-}
 
 document.addEventListener('keydown', (e) => {
-    let playKey = new Switcher(getAudioSrc(keySoundsRef, e.key), 5)
-
-    if(mapedKeys.includes(e.key)) {
-
-        playKey.play();
-    }
-    
-
-    playedKeys = 0
+    playTune(e.key)
 })
 
 const handleVolume = (e) => {
